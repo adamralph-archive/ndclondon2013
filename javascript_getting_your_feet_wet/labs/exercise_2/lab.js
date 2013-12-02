2241a0bbb79272de7281e17bec0cdae9dc293e93
venkats@agiledeveloper.com <venkats@agiledeveloper.com@1e81dee0-2241-4859-a8c8-ee16f12c0b5b>
2013-12-02 11:22:09 +0000

    lab 2
    git-svn-id: https://www.agiledeveloper.com/svn/code/ndclondon2013@660 1e81dee0-2241-4859-a8c8-ee16f12c0b5b

---
+++ javascript_getting_your_feet_wet/labs/exercise_2/lab.js
@@ -0,0 +1,18 @@
+//Implement greet in a way that it takes 
+//no arguments at all in the parameter list.
+
+???
+
+var invokeGreetWith = function(message, names) {
+  ???
+}
+
+invokeGreetWith('Hello', ['Jake', 'Jane', 'Curt'])
+invokeGreetWith('Howdy', ['Jack', 'Jill'])
+invokeGreetWith('Howdy', ['Bill'])
+
+/*
+Hello Jake, Jane, and Curt
+Howdy Jack and Jill
+Howdy Bill
+*/
