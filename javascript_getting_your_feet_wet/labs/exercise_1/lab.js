e221bdafb68b46f24d18ea497309a2b4afe546af
Adam Ralph <adam@adamralph.com>
2013-12-02 10:05:04 +0000

    lab 1

--- javascript_getting_your_feet_wet/labs/exercise_1/lab.js
+++ javascript_getting_your_feet_wet/labs/exercise_1/lab.js
@@ -1,6 +1,8 @@
+"option strict";
+
 function canDrive(person, age) {
   var message = "";
-  if(age == 16)
+  if(age === 16)
     message = "can drive under supervision";
   else {
     if(age < 17)
@@ -13,22 +15,22 @@ function canDrive(person, age) {
 }
 
 function printForEachPersonIn(people) {
-  for(i = 0; i < people.length; i++) {
+  for(var i = 0; i < people.length; i++) {
     canDrive(people[i][0], people[i][1]);
   }
 }
 
 function printInfoForEachFamilyIn(family) {
-  for(i = 0; i < family.length; i++) {
+  for(var i = 0; i < family.length; i++) {
     console.log("Last names: " + family[i][0])
     printForEachPersonIn(family[i][1]);
   }  
 }
 
-families = [
+var families = [
  ['Smiths', [['Jake', 10], ['John', 19]]],
  ['Johnson', [['Jill', 16], ['Jane', 18]]],
- ['Williams', [['Drew', "@16"], ['Rachel', 20]]]
+ ['Williams', [['Drew', 16], ['Rachel', 20]]]
 ]
 
 printInfoForEachFamilyIn(families);
