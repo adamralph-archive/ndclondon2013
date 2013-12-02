d5ace9e6d90c9cb4c2ea8cdb67337deae2e82898
Adam Ralph <adam@adamralph.com>
2013-12-02 14:12:44 +0000

    lab 3

--- javascript_getting_your_feet_wet/labs/exercise_3/lab.js
+++ javascript_getting_your_feet_wet/labs/exercise_3/lab.js
@@ -1,14 +1,18 @@
-???
+"use strict";
+var names = [ 'John', 'Jake', 'Bradley', 'Kim', 'Kate', 'Sam' ];
 
-//console.log("All names with four letters:");
-//console.log(???);
-//
-//console.log("All names with three letters:");
-//console.log(???);
-//
-//console.log("All names that start with 'K':");
-//console.log(???);
-//
-//console.log("All names that start with 'J':");
-//console.log(???);
+var message = function(selector) {
+	return names.filter(selector).join(', ')
+}
 
+console.log("All names with four letters:");
+console.log(message(function (name) { return name.length === 4; }));
+
+console.log("All names with three letters:");
+console.log(message(function (name) { return name.length === 3; }));
+
+console.log("All names that start with 'K':");
+console.log(message(function (name) { return name.indexOf('K') === 0; }));
+
+console.log("All names that start with 'J':");
+console.log(message(function (name) { return name.indexOf('J') === 0; }));
