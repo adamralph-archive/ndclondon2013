9fb908ff2c1689eab7b54ca1f05dd60312397781
venkats@agiledeveloper.com <venkats@agiledeveloper.com@1e81dee0-2241-4859-a8c8-ee16f12c0b5b>
2013-12-02 13:54:08 +0000

    lab 3
    git-svn-id: https://www.agiledeveloper.com/svn/code/ndclondon2013@670 1e81dee0-2241-4859-a8c8-ee16f12c0b5b

---
+++ javascript_getting_your_feet_wet/labs/exercise_3/lab.js
@@ -0,0 +1,14 @@
+???
+
+//console.log("All names with four letters:");
+//console.log(???);
+//
+//console.log("All names with three letters:");
+//console.log(???);
+//
+//console.log("All names that start with 'K':");
+//console.log(???);
+//
+//console.log("All names that start with 'J':");
+//console.log(???);
+
