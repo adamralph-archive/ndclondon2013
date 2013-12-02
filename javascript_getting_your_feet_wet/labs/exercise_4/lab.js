137e62618d287c493847f9b60f60dd1ee5183e6b
Adam Ralph <adam@adamralph.com>
2013-12-02 15:54:56 +0000

    lab 4

--- javascript_getting_your_feet_wet/labs/exercise_4/lab.js
+++ javascript_getting_your_feet_wet/labs/exercise_4/lab.js
@@ -1,6 +1,17 @@
-???
+var PalindromeChecker = function() {
+  this.isSpecial = function(word) {
+	return word.split("").reverse().join("") === word;
+  }
+}
 
 var palindromeChecker = new PalindromeChecker();
+
+var printIfWordIsSpecial=function(checker, word) {
+	if (checker.isSpecial(word))
+		console.log(word + " is special");
+	else
+		console.log(word + " is not special");
+}
 
 printIfWordIsSpecial(palindromeChecker, 'mom');
 printIfWordIsSpecial(palindromeChecker, 'dad');
