d6b3b3b9d2a76ace102b93f7de5cd544b12f5dfe
venkats@agiledeveloper.com <venkats@agiledeveloper.com@1e81dee0-2241-4859-a8c8-ee16f12c0b5b>
2013-12-02 09:35:31 +0000

    right way to check equals and inequality
    git-svn-id: https://www.agiledeveloper.com/svn/code/ndclondon2013@644 1e81dee0-2241-4859-a8c8-ee16f12c0b5b

---
+++ javascript_getting_your_feet_wet/examples/part1/5_right_way_to_check_equals_and_inequality/sample.js
@@ -0,0 +1,20 @@
+//if a is equal to b, and
+//if b is equal to c, then
+//we can say that a is equal to c.
+
+console.log("1" == 1);
+console.log(1 == "1.0");
+console.log("1" == "1.0");
+
+//don't use == to compare.
+console.log('-------');
+console.log("1" === 1);
+console.log(1 === "1.0");
+console.log("1" === "1.0");
+
+//to compare use === instead of ==.
+
+//Likewise, don't use != instead use
+//!== for not equals comparison.
+
+// ===  and !==
