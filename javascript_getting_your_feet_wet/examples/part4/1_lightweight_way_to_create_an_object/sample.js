4654d6efd865c6215a80153a65b9e5d33730c9af
venkats@agiledeveloper.com <venkats@agiledeveloper.com@1e81dee0-2241-4859-a8c8-ee16f12c0b5b>
2013-12-02 14:27:27 +0000

    lightweight way to create an object
    git-svn-id: https://www.agiledeveloper.com/svn/code/ndclondon2013@672 1e81dee0-2241-4859-a8c8-ee16f12c0b5b

---
+++ javascript_getting_your_feet_wet/examples/part4/1_lightweight_way_to_create_an_object/sample.js
@@ -0,0 +1,12 @@
+//JSON
+var person = {
+  first: 'John',
+  last: 'Smith',
+  age: 2,
+  play: function() { console.log('playing...'); }
+};
+
+console.log(person);
+console.log(person.first);
+console.log(person.age);
+person.play();
