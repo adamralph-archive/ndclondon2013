7d24a2b6769862cabe4a6a290db4364b56c32ccc
Adam Ralph <adam@adamralph.com>
2013-12-02 12:01:20 +0000

    lab 2

--- javascript_getting_your_feet_wet/labs/exercise_2/lab.js
+++ javascript_getting_your_feet_wet/labs/exercise_2/lab.js
@@ -1,10 +1,29 @@
 //Implement greet in a way that it takes 
 //no arguments at all in the parameter list.
 
-???
+"use strict";
+var greet = function() {
+  var greeting = "";
+  for (var i=0; i < arguments.length; i++) {
+	if (i === arguments.length - 1) {
+	  greeting += arguments[i];
+	}
+	else if (arguments.length == 2 && i === (arguments.length - 2)) {
+	  greeting += arguments[i] + " and ";
+	}
+	else if (i === (arguments.length - 2)) {
+	  greeting += arguments[i] + ", and ";
+	}
+	else {
+	  greeting += arguments[i] + ", ";
+	}
+  }
+  
+  console.log(this + ' ' + greeting);
+}
 
 var invokeGreetWith = function(message, names) {
-  ???
+  greet.apply(message, names);
 }
 
 invokeGreetWith('Hello', ['Jake', 'Jane', 'Curt'])
